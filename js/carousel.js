8002ec4bea88ac50bdac49f8142a47b7c2e5a8ba
Dave Mesde <dmesde@dxc.com>
2019-07-05 16:55:42 +0800

    Commit with files

---
+++ js/carousel.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instance = M.Carousel.init(elems);
+    var instance = M.Carousel.init({numVisible:2});
+    
+  });
+
